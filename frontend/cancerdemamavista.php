ffba0e2914d31c03fc3a9bfae66e808013628337
Sandra-OP <124931474+Sandra-OP@users.noreply.github.com>
2023-02-09 15:55:33 -0600

    Se tilda la a

--- frontend/cancerdemamavista.php
+++ frontend/cancerdemamavista.php
@@ -11,7 +11,7 @@
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css' rel='stylesheet'>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <!--<script defer src="https://app.embed.im/snow.js"></script>-->
-    <title>Cancer de Mama</title>
+    <title>Cáncer de Mama</title>
 </head>
 
 <body>
